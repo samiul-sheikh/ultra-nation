2393ad6e0dfa4b0c49ef1169341935b6e45262f5
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-28 15:12:21 +0600

    display loaded data in a simple way

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
@@ -10,27 +9,25 @@ function App() {
     useEffect(() => {
         fetch('https://restcountries.eu/rest/v2/all')
             .then(res => res.json())
-            .then(data => setCountries(data))
+            .then(data => {
+                // set data in state
+                setCountries(data);
+                console.log(data)
+                // display country name in console as array from object
+                const names = data.map(country => country.name)
+                console.log(names);
+            })
+            .catch(error => console.log('Error Found in Data Load!'))
     }, [])
     return (
         <div className="App">
-            
             <h1>Country Loaded: {countries.length}</h1>
-
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-        </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-        </a>
-            </header>
+            {/* display country name in UI as list item */}
+            <ul>
+                {
+                    countries.map(country => <li>{country.name}</li>)
+                }
+            </ul>
         </div>
     );
 }
